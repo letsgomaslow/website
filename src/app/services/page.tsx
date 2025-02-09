f91dd49ebbda372392713eb18905be80e7ed1273
Rakesh David <kesh@Rakeshs-Mac-mini.local>
2025-02-08 23:52:20 -0500

    Task 8: TRiSM Framework section implementation complete

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { GenerativeAIStrategySection } from "@/components/services/GenerativeAIStrategySection";
 import { OpenSourceSection } from "@/components/services/OpenSourceSection";
+import { TRiSMSection } from "@/components/services/TRiSMSection";
 
 export default function ServicesPage() {
   return (
     <div className="flex flex-col">
       <GenerativeAIStrategySection />
       <OpenSourceSection />
+      <TRiSMSection />
     </div>
   );
 } 
